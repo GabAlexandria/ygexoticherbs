1800f0e484c847c359e69a75aa6783be88351b06
GabAlexandria <gabagrant@gmail.com>
2012-11-12 22:51:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ygexoticherbs::Application.config.secret_token = '87d81738282e6d6ceb221207994c23fedfc850a4078b7b70fb7a55080c8c5f1b028e428a83f05ecfbff6e8ee9aadc7d1a321fcd1bff64a2f4434b68601a2d4f4'
