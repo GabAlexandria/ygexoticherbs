e5a14bb718703f7c43241fc52a68e640fe3491ca
GabAlexandria <gabagrant@gmail.com>
2012-12-06 19:10:32 -0500

    setting up staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate_user
+
   helper_method :current_user
 
   private
@@ -13,5 +15,13 @@ class ApplicationController < ActionController::Base
   def current_user
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.record
+  end
+
+  def authenticate_user
+    if Rails.env == "staging"
+      authenticate_or_request_with_http_basic do |name, password|
+        name == 'ygadmin' && password == '1qaz7ujm'
+      end
+    end
   end
 end
