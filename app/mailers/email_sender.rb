dc6752e5106ed0a681956c14cca4f04718130a36
GabAlexandria <gabagrant@gmail.com>
2013-02-05 15:21:45 -0500

    disable mailer

--- app/mailers/email_sender.rb
+++ app/mailers/email_sender.rb
@@ -1,8 +1,8 @@
 class EmailSender < ActionMailer::Base
-  default :from => "prod@ygherbs.com"
+  #default :from => "prod@ygherbs.com"
 
-  def contact_us_emails(user)
-    @user = user
-    mail (:to => user.email, :subject => "New messages from potential clients")
-  end
+  #def contact_us_emails(user)
+   # @user = user
+   # mail (:to => user.email, :subject => "New messages from potential clients")
+  #end
 end
